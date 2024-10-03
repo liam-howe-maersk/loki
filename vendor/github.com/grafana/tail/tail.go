7aec6f7697e4546b9d7c3ee673064a7a0621c4d6
madhu-reddy-peram <madhu.r@maersk.com>
2024-10-03 10:47:39 +0100

    increment line count for app line only

--- vendor/github.com/grafana/tail/tail.go
+++ vendor/github.com/grafana/tail/tail.go
@@ -345,11 +345,11 @@ func (tail *Tail) tailFileSync() {
 
 		// Process `line` even if err is EOF.
 		if err == nil {
-			lineCount++
+			if !strings.Contains(tail.Filename, "promtail-liam-test-logs") {
+				lineCount++
+			 }
 			cooloff := !tail.sendLine(line+ fmt.Sprintf(" lineCount=%d", lineCount))
-			// if !strings.Contains(tail.Filename, "promtail-liam-test-logs") {
-			// 	tail.Logger.Printf("Log line(s) read and sent for file %s, with cooloff %v, contains 'http /routings-queries' %v\n", tail.Filename, cooloff, strings.Contains(line, "http /routings-queries"))
-			// }
+
 			if cooloff {
 				// Wait a second before seeking till the end of
 				// file when rate limit is reached.
